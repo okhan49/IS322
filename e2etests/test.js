2fcc9e0937fcb9868e964ed4367f46a1a7865c14
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-05-03 16:51:55 -0400

    wrote word submission e2e test

--- e2etests/test.js
+++ e2etests/test.js
@@ -1,3 +1,4 @@
+/* global describe, it, browser */
 const expect = require('chai').expect;
 
 describe('Last Letter Game App', () => {
@@ -6,5 +7,14 @@ describe('Last Letter Game App', () => {
         const actualTitle = browser.getTitle();
 
         expect(actualTitle).to.eql('Last Letter Game');
+    });
+    it('Should allow me to submit an animal', () => {
+        const wordText = 'Zebra';
+        browser.url('http://localhost:3000/');
+        browser.element('.word-input').setValue(wordText);
+        browser.click('.word-submit');
+        const actual = browser.element('.word-text').getText();
+
+        expect(actual).to.equal(wordText);
     });
 });
