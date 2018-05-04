4a008fb4dd85bd2cf279d01e23082a04e127618f
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-05-03 21:49:13 -0400

    added winner to the state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,14 +4,23 @@ import Animals from '../animals';
 export const initialState = {
     guessedAnimals:[],
     animalsBank: Animals,
+    winner: "none",
 };
 
 export const reducer = (state = initialState, action) => {
+    var win = state.winner;
     switch (action.type){
         case types.SUBMIT_ANIMAL:
             if(state.animalsBank[action.text.toLowerCase()[0]].length != 0){
                 var animalsCopy = Object.assign(state.animalsBank);
                 let currLet = action.text.toLowerCase()[0];
+                if (state.guessedAnimals.length != 0){
+                    let compLastLetter = state.guessedAnimals[state.guessedAnimals.length - 1].text.toLowerCase().substr(state.guessedAnimals[state.guessedAnimals.length - 1].text.length -1);
+                    if(compLastLetter != currLet){
+                        console.log("You Must Enter a word that begins with '" + compLastLetter.toUpperCase() + "'");
+                        win = "Computer";
+                    }
+                }
                 let index = animalsCopy[currLet].indexOf(action.text.toLowerCase());
                 if(index !== -1){
                     animalsCopy[currLet].splice(index, 1);
@@ -20,6 +29,10 @@ export const reducer = (state = initialState, action) => {
                         var randWord = animalsCopy[lastLetter][Math.floor(Math.random() * animalsCopy[lastLetter].length)];
                         let idx = animalsCopy[lastLetter].indexOf(randWord);
                         animalsCopy[lastLetter].splice(idx, 1);
+                    }
+                    else {
+                        console.log("You Win");
+                        win = "Player";
                     }
                     return {
                         ...state,
@@ -35,10 +48,12 @@ export const reducer = (state = initialState, action) => {
                             },
                         ],
                         animalsBank: animalsCopy,
+                        winner: win,
                     };
                 }
                 else {
                     console.log("That's not a valid animal");
+                    win = "Computer";
                 }
             }
             else {
@@ -51,9 +66,11 @@ export const reducer = (state = initialState, action) => {
                 }
                 if (alreadyGuessed){
                     console.log("You already guessed that animal");
+                    win = "Computer";
                 }
                 else {
                     console.log("That's not a valid animal");
+                    win = "Computer";
                 }
             }
             return {
@@ -66,6 +83,7 @@ export const reducer = (state = initialState, action) => {
                     },
                 ],
                 animalsBank: state.animalsBank,
+                winner: win,
             };
         default:
             return state;
